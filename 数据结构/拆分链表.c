f4aa362a07766984ca40e8ef1664c62d0b684e0e
不二臣丶M <45445334+Ms27520@users.noreply.github.com>
2021-11-24 20:33:31 +0800

    haha

--- 数据结构/拆分链表.c
+++ 数据结构/拆分链表.c
@@ -1,6 +1,19 @@
 void decompose(Linklist La, Linklist &Lb, Linklist &Lc){
-     Linknode *p;
-     Lc = (Linknode *)malloc(sizeof(Linknode));
-     Lc->next = NULL;
-     
+    Linknode *p;
+    Lc = (Linknode *)malloc(sizeof(Linknode));
+    Lc->next = NULL;
+    p = La->next;
+    Lb = La;
+    Lb->next = NULL;
+    while(p){
+        La = p->next;
+        if(p->data > 0){
+            p->next Lc->next;
+            Lc->next = p;
+        }else{
+            p->next = Lb->next;
+            Lb->next = p;
+        }
+        p = La;
+    }
  }
